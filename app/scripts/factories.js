b92f963f3d49dc8d07980d2d1b21dac71d29fd14
Dudley Dickerson <littledudz@yahoo.com>
2017-05-20 14:54:36 -0500

    working on navbar

--- app/scripts/factories.js
+++ app/scripts/factories.js
@@ -1,5 +1,29 @@
 angular.module('WebApp')
 
-.service(
+.service ( // Guest list yes array
     
+)
+
+.service ( // Guest list no array
+    
+)
+
+.service ( // Name check when clicking yes or no
+
+)
+
+.service ( // Button disappear and text insert
+
+)
+
+.service ( // Number of guest & name check
+
+)
+
+.service ( // Total guest list number array
+
+)
+
+.service ( // Addition function
+
 )
