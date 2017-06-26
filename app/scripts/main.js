29c15bdd0711872cd944e903323af3e66a2b12df
Dudley Dickerson <littledudz@yahoo.com>
2017-06-26 15:28:28 -0500

    bug fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@ var app = angular.module('WebApp', ['ngRoute', 'ngResource']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
   .when('/', {
-    templateUrl:'snippets/views/home.html',
+    templateUrl:'snippets/home.html',
     controller:'HomeViewController'
   })
   .otherwise({
